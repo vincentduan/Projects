58856e1e43076942611eb0a99f7ba4be8f6c730b
duandingyang <duandingyang@bjtu.edu.cn>
2016-10-09 21:02:43 +0800

    添加File输出

--- Java_Project/src/main/java/com/vincent/LTP/ReadXML.java
+++ Java_Project/src/main/java/com/vincent/LTP/ReadXML.java
@@ -1,6 +1,10 @@
 package com.vincent.LTP;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.List;
 
 import org.dom4j.Document;
@@ -22,15 +26,15 @@ public class ReadXML {
 
         // 获取根元素
         Element root = document.getRootElement();
-        System.out.println("Root: " + root.getName());
+        //System.out.println("Root: " + root.getName());
 
         // 获取所有子元素
         List<Element> childList = root.elements();
-        System.out.println("total child count: " + childList.size());
+        //System.out.println("total child count: " + childList.size());
 
         // 获取特定名称的子元素
         List<Element> doc = root.elements("doc");
-        System.out.println("doc child: " + doc.size());
+        //System.out.println("doc child: " + doc.size());
         
         // 获取名字为指定名称的第一个子元素
         Element firstDocElement = root.element("doc");
@@ -47,36 +51,43 @@ public class ReadXML {
         		int word_id = 0;
         		for (Element word : words){
         			word_id = Integer.parseInt(word.attributeValue("id"));
-        			String word_cont = word.attributeValue("cont");
-        			String word_ne = word.attributeValue("ne");
-        			String word_semrelate = word.attributeValue("semrelate");
-        			System.out.println("para:"+para_id + ",sentence:"+sentence_id + ",word_id:" + word_id + ",word_cont:"+word_cont + ",word_ne:"+word_ne + ",semrelate:" + word_semrelate);
+        			String word_cont = word.attributeValue("cont");//单词内容
+        			String word_ne = word.attributeValue("ne");//命名实体识别
+        			String word_semparent = word.attributeValue("semparent");//语义依存分析父节点id
+        			String word_semrelate = word.attributeValue("semrelate");//语义依存分析相应关系
+        			if(word_ne.contains("Nh") || word_ne.contains("Ns") || word_ne.contains("Ni") || word_semrelate.contains("Tmod") || word_semrelate.contains("Time")){
+        				System.out.println("para:"+para_id + ",sentence:"+sentence_id + ",word_id:" + word_id + ",word_cont:"+word_cont + ",word_ne:"+word_ne + ",word_semparent:" + word_semparent + ",semrelate:" + word_semrelate);
+        				doFormat(para_id+"", sentence_id+"", word_id+"", word_cont, word_ne, word_semparent, word_semrelate);
+        			}
+        			if(!word_ne.equals("O")){
+        				
+        			}
+        			
         		}
         	}
         }
-        /*System.out.println("迭代输出-----------------------");
-        // 迭代输出
-        for (Iterator iter = root.elementIterator(); iter.hasNext();)
-        {
-            Element e = (Element) iter.next();
-            System.out.println(e.attributeValue("name"));
-
-        }
-
-        System.out.println("用DOMReader-----------------------");
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        DocumentBuilder db = dbf.newDocumentBuilder();
-        // 注意要用完整类名
-        org.w3c.dom.Document document2 = db.parse(new File("students.xml "));
-
-        DOMReader domReader = new DOMReader();
-
-        // 将JAXP的Document转换为dom4j的Document
-        Document document3 = domReader.read(document2);
-
-        Element rootElement = document3.getRootElement();
-
-        System.out.println("Root: " + rootElement.getName());*/
-
 	}
+	
+	public static void doFormat(String para_id, String sentence_id, String word_id, String word_cont, String word_ne, String word_semparent, String word_semrelate) throws Exception{
+		FileOutputStream fos = new FileOutputStream("D://ollie//result.txt", true);
+		byte[] para_id_byte = ("段落id：" + para_id + "\n").getBytes();
+		fos.write(para_id_byte);
+		byte[] sentence_id_byte = ("\t句子id：" + sentence_id + "\n").getBytes();
+		fos.write(sentence_id_byte);
+		byte[] word_id_byte = ("\t\t单词id：" + word_id + "\n").getBytes();
+		fos.write(word_id_byte);
+		byte[] word_cont_byte = ("\t\t实体：" + word_cont + "\n").getBytes();
+		fos.write(word_cont_byte);
+		byte[] word_ne_byte = ("\t\t实体识别：" + word_ne + "\n").getBytes();
+		fos.write(word_ne_byte);
+		byte[] word_semrelate_byte = ("\t\t语义依存分析：" + word_semrelate + "\n").getBytes();
+		fos.write(word_semrelate_byte);
+	    fos.close();
+	}
+	
+	public byte[] writeDate(String str) throws Exception{
+		byte[] data = str.getBytes();
+    	return data;
+	}
+	
 }
